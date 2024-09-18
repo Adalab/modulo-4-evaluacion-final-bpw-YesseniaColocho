38e18ce4e2c329347a2d9e31e4f91e23f8381c54
YesseniaColocho <Colocho.yessenia@gmail.com>
2024-09-18 12:50:52 +0200

    Error catch implement

--- src/index.js
+++ src/index.js
@@ -24,74 +24,117 @@ async function getBDconnection() {
 }
 
 server.get("/videogames", async (req, res) => {
-  const connection = await getBDconnection();
-  const query = "SELECT * FROM videogame";
-  const [result] = await connection.query(query);
-  await connection.end();
+  try {
+    const connection = await getBDconnection();
+    const query = "SELECT * FROM videogame";
+    const [result] = await connection.query(query);
+    await connection.end();
 
-  res.status(200).json({
-    info: { cont: result.leght },
-    result: result,
-  });
+    res.status(200).json({
+      info: { cont: result.leght },
+      result: result,
+    });
+  } catch (error) {
+    res.status(500);
+    res.json({
+      success: false,
+      error,
+    });
+  }
 });
 
 server.post("/videogames", async (req, res) => {
-  const { name, description, release_date, score } = req.body;
-  const connection = await getBDconnection();
-  const query =
-    "INSERT INTO videogame (name, description, release_date, score) VALUES (?,?,?,?)";
-  const [result] = await connection.query(query, [
-    name,
-    description,
-    release_date,
-    score,
-  ]);
+  try {
+    const { name, description, release_date, score } = req.body;
+    const connection = await getBDconnection();
+    const query =
+      "INSERT INTO videogame (name, description, release_date, score) VALUES (?,?,?,?)";
+    const [result] = await connection.query(query, [
+      name,
+      description,
+      release_date,
+      score,
+    ]);
 
-  res.status(201).json({
-    status: true,
-    id: result.insertId,
-  });
+    res.status(201).json({
+      status: true,
+      id: result.insertId,
+    });
+    await connection.end();
+  } catch (error) {
+    res.status(500);
+    res.json({
+      success: false,
+      error,
+    });
+  }
 });
 
 server.put("/videogames/:id", async (req, res) => {
-  const { name, description, release_date, score } = req.body;
-  const id = req.params.id;
-  const connection = await getBDconnection();
-  let query = "UPDATE videogame SET ";
+  try {
+    const { name, description, release_date, score } = req.body;
+    const id = req.params.id;
+    const connection = await getBDconnection();
+    let query = "UPDATE videogame SET ";
 
-  if (name) {
-    query += `name = '${name}'`;
-    if (description || release_date || score) {
-      query += ", ";
+    if (name) {
+      query += `name = '${name}'`;
+      if (description || release_date || score) {
+        query += ", ";
+      }
     }
-  }
-  if (description) {
-    query += `description = '${description}'`;
-    if (release_date || score) {
-      query += ", ";
+    if (description) {
+      query += `description = '${description}'`;
+      if (release_date || score) {
+        query += ", ";
+      }
     }
-  }
-  if (release_date) {
-    query += `release_date = '${release_date}'`;
+    if (release_date) {
+      query += `release_date = '${release_date}'`;
+      if (score) {
+        query += ", ";
+      }
+    }
     if (score) {
-      query += ", ";
+      query += `score = ${score}`;
     }
+
+    query += ` WHERE id = ${id}`;
+
+    console.log(query);
+    const [result] = await connection.query(query);
+
+    res.status(201).json({
+      status: true,
+      id,
+    });
+    await connection.end();
+  } catch (error) {
+    res.status(500);
+    res.json({
+      success: false,
+      error,
+    });
   }
-  if (score) {
-    query += `score = ${score}`;
-  }
-
-  query += ` WHERE id = ${id}`;
-
-  console.log(query);
-  const [result] = await connection.query(query);
-
-  res.status(201).json({
-    status: true,
-    id,
-  });
-
-  await connection.end();
 });
 
+server.delete("/videogames/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const connection = await getBDconnection();
+    const query = "DELETE FROM videogame WHERE id=?";
+    const [result] = await connection.query(query, [id]);
 
+    res.status(201).json({
+      status: true,
+      id,
+    });
+    await connection.end();
+  } catch (error) {
+    res.status(500);
+    res.json({
+      success: false,
+      error,
+    });
+  }
+});
